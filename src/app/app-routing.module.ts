5856b7ff6a3e659850b1ee54fa977bd92b27cf9c
yamatoto <mftb35ymt@yahoo.co.jp>
2020-02-06 01:36:29 +0900

    AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 
 const appRoutes: Routes = [
     {
         path: 'home',
         loadChildren: () => import('./modules/homes/homes.module').then(m => m.HomesModule),
+        canActivate: [AuthGuard],
     },
     {
         path: 'products',
         loadChildren: () =>
             import('./modules/products/products.module').then(m => m.ProductsModule),
+        canActivate: [AuthGuard],
     },
     {
         path: 'orders',
         loadChildren: () => import('./modules/orders/orders.module').then(m => m.OrdersModule),
+        canActivate: [AuthGuard],
     },
     // {
     //     path: 'maintenamce',
